8f4353beec12acf5a40d68dc7f647750bcd5550f
Nattapong Swrs <tar_afaps@outlook.com>
2021-06-09 12:06:30 +0700

    save config

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-import express from 'express';
-
-const app = express();
+var express = require('express')
+var app = express()
 const port = 8080;
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+
+app.get('/',(req, res) => {
   res.render("index", {});
 });
-app.get('/test', (req, res) => {
-  res.send('FUCK')
+app.post('/noti_line',(req, res) => {
+  console.log(req.body);
+  // let lineNotify = require('line-notify-nodejs')(req.body.token);
+  // lineNotify.notify({
+  //   message: req.body.message,
+  // }).then(() => {
+  //   res.send('complete')
+  // });
 });
+
+
+
 app.listen(port,  "0.0.0.0");
 console.log("Starting Server. port "+port);
 console.log("http://localhost:"+port);
