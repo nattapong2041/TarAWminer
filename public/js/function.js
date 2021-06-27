149d823132fc7c2c9a277e3a4e6c77e55e81edf0
Nattapong Swrs <tar_afaps@outlook.com>
2021-06-27 23:06:01 +0700

    change default to pink

--- public/js/function.js
+++ public/js/function.js
@@ -1,6 +1,6 @@
 const base_api = [
+    'https://wax.pink.gg', 
     'https://wax.greymass.com',
-    'https://wax.pink.gg',  
     'https://wax.cryptolions.io',
     'https://wax.dapplica.io',
     //'https://chain.wax.io',
