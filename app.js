7f30a90dc9a745508e872fb47e01b48b29bb96f1
Nattapong Swrs <tar_afaps@outlook.com>
2021-06-28 01:58:17 +0700

    temp

--- app.js
+++ app.js
@@ -9,6 +9,26 @@ const { StaticPool } = require("node-worker-threads-pool");
 var app = express()
 const port = 8080;
 var oldNonce = new Map();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const base_api = [
     'https://wax.greymass.com',
     'https://wax.pink.gg',
