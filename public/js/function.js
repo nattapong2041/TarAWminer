6fe1673ac07d9531771add9fdefd733a244049aa
Nattapong Swrs <tar_afaps@outlook.com>
2021-06-30 00:39:38 +0700

    new serrver

--- public/js/function.js
+++ public/js/function.js
@@ -663,7 +663,7 @@ async function self_mine(account) {
 const lazy_server_mine = async (account) => {
     const ninja = ['Rate', 'rate', 'Limit', 'limit']
     console.log('Mining with lazy server');
-    let url = `https://lazydigger.as.r.appspot.com/mine_worker?account=${account}`;
+    let url = `http://139.180.216.175:8080/mine_worker?account=${account}`;
     try {
         return await fetch(url)
             .then((response) => {
