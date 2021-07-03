a68432d87739ebebc3ae46f1eb2b878ce1ca9bc7
OopOopu <33390164+OopOopu@users.noreply.github.com>
2021-07-03 08:03:37 +0700

    first deploy
    -0-

--- adddb.py
+++ adddb.py
@@ -1,27 +1,25 @@
+from flask.json import jsonify
 import pymongo
 from pymongo import MongoClient
 import json
 import datetime as dt
 import time
-
+from hashlib import sha256
 client = MongoClient('mongodb://localhost:27017/dbtests')
 db = client.dbtests
 db.test.create_index([('username', pymongo.ASCENDING)], unique=True)
 db.testcode.create_index([('code', pymongo.ASCENDING)], unique=True)
+db.testvip.create_index([('wam', pymongo.ASCENDING)], unique=True)
 
 def adduser():
     try:
         db.test.insert_one(
             {
-                'name':'test01',
-                'username':'ooppu',
+                'username':'aaaa',
                 'password': 'ooppy',
-                'email': 'eiei@gmail.com',
-                'aid':["eiei.wam"],
-                'idcount': 1 ,
-                'nonce':["non"],
-                'vip':[0]
-            })
+                'wid':[{'id':"eiei.wam",'vip':"0"},{'id':"sss.wam",'vip':"0"}]
+            }
+        )
     except pymongo.errors.DuplicateKeyError:
             print("dup username")
 
@@ -29,10 +27,11 @@ def addcode():
     try:
         db.testcode.insert_one(
             {
-                'code':'aaaaaaaa',
-                'maxnuse': [0,0],
-                'date': dt.datetime.today(),
-                'active' : 'x',
+                'code':'lazy-01-50',
+                'hash':'0cb68ad96dbbe62f9c23af0f58abe3eb57c06713a2c4aba777c2018dd8d93eb2',
+                'wam': [],
+                'count': 50,
+                'start' : dt.datetime.today(),
                 'stop' : 'x',
             }
         )
@@ -52,6 +51,74 @@ def dat2():
     else:
         print(dataA)
 
+def update():
+    db.test.update_one({'username' : 'aaaa'},{'$push':{'wid':{'id':"eieiman.wam",'vip':"0"}}} )
+
+def addwam():
+    db.test.update_one({'username' : 'aaaa'},{'$push':{'wid':{'id':'oiuy.wam','vip':"0"}}} )
+
+def delet():
+    db.test.update({'username' : 'aaaa'},{'$pull':{'wid':{'id':"eieiman.wam"}}} )
+
+def addnonce():
+    #dataA = db.test.find_one({"username": "saad"},{'wid.nonce':"eieiman.wam"})
+
+    db.test.update_one({'$and': [{'username':'saad'},{'wid.id':"eiei.wam"}]},{'$set':{'wid.$.nonce':'safgdsagsdfg'}})
+    #dataA = db.test.find_one({'$and': [{'username':'aaaa'},{'wid.id':"eiei.wam"}]})
+
+def addvipwam():
+    for x in range(10):
+
+        try:
+            db.testvip.insert_one(
+                {
+                    'wam':'test0'+ str(x),
+                    'nonce':'default',
+                }
+            )
+        except pymongo.errors.DuplicateKeyError:
+                print("dup wam")
+def testhash():
+    input = ('lazy-01-50')
+    hashe = sha256(input.encode('utf-8')).hexdigest()
+    print(hashe)
+    hashe = hashe[:13]
+    print(hashe)
+
+def find13():
+    code = '0cb68ad96dbbe'
+    dataA = db.testcode.find_one({ 'hash': { '$regex': code } })
+    if dataA is None:
+        print("code pid")
+    else:
+        count = dataA['count']
+        leng = len(dataA['wam'])
+        if leng >= count :
+            print('full vip')
+        else : 
+            db.testcode.update_one({ 'hash': { '$regex': code } },{'$push':{'wam':'123.wam'}})
+        print(leng)
+        print(dataA)
+
+def reqnonce():
+    wid = 'test05'
+    dataA = db.testvip.find_one({"wam": wid })
+    print(json.dumps(dataA['nonce']))
+
+def reqwam ():
+    dataA = db.test.find_one({'username':'aaaa'})
+    yo = dataA['wid']
+    json.dumps(yo)
+    print(yo)
+    print(dataA['wid'])
+
+def teste():
+    yo = ["wqobq.wam","1234a.wam"]
+    for i in range(len(yo)) :
+        print(i)
+
+
+
 # In[ ]:
 if __name__ == '__main__':
-    dat2()  
+    teste()  
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request,jsonify, url_for, redirect, session
+from flask import Flask, render_template, request,jsonify, url_for, redirect, session,Response
 import pymongo
 from pymongo import MongoClient
-import bcrypt
+from hashlib import sha256
+import json
 
+from pymongo import message
 client = MongoClient('mongodb://localhost:27017/dbtests')
 db = client.dbtests
 
@@ -10,54 +12,50 @@ db = client.dbtests
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+
+
+
+
 @app.route("/register", methods=["POST", "GET"])
 
 def register():
     if request.method == "POST":
-        nickname = request.form.get("name")
         user = request.form.get("username")
-        email = request.form.get("email")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
         #if found in database showcase that it's found 
         user_found = db.test.find_one({"name": user})
-        email_found = db.test.find_one({"email": email})
-        if user_found:
+        if user_found is not None:
             message = 'There already is a user by that name'
-            return jsonify({"error":message})
+            return Response(message, status=500)
             #return render_template('index.html', message=message)
-        if email_found:
-            message = 'This email already exists in database'
-            return jsonify({"error":message})
         if password1 != password2:
             message = 'Passwords should match!'
-            return jsonify({"error":message})
+            return Response(message, status=500)
         else:
             #hash the password and encode it
             ###hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
             #assing them in a dictionary in key value pairs
             ###user_input = {'name': user, 'email': email, 'password': hashed}
             #insert it in the record collection
-            db.test.insert_one(
+            try :db.test.insert_one(
             {
-                'name':nickname,
                 'username': user,
                 'password': password2,
-                'email': email
             })
-            
+            except :
+                return Response("duplicate id", status=500)
             
             #find the new created account and its email
-            user_data = db.tests.find_one({"email": email})
-            new_user = user_data['email']
+            #user_data = db.tests.find_one({"email": email})
             #if registered redirect to logged in as the registered user
-            return jsonify({"error":"testestest"})
+            return Response("regissuccess", status=200)
             #return render_template('logged_in.html', username=new_user)
-    return render_template('index.html')
+    return Response("{'a':'b'}", status=500)
 
-@app.route('/logged_in')
+#@app.route('/logged_in')
 
-def logged_in():
+#def logged_in():
     if "username" in session:
         username = session["username"]
         return render_template('logged_in.html', username=username)
@@ -71,9 +69,9 @@ def logged_in():
 def login():
     message = 'Please login to your account'
     #ถ้ามีsession ยุแล้วจะกลับไปหน้าล็อคอินแล้ว
-    if "username" in session:
+    #if "username" in session:
 
-        return jsonify({"123":"123"})
+     #   return jsonify({"123":"123"})
         #return redirect(url_for("logged_in"))
     #กดล็อคอินมา
     if request.method == "POST":
@@ -88,85 +86,147 @@ def login():
             
             if passwordcheck == password :
             #if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
-                session["username"] = user_val
-                
-                return redirect(url_for('logged_in'))
+                #session["username"] = user_val
+                message = 'Log in success'
+                return Response(message, status=200)
             else:
-                if "username" in session:
-                    return redirect(url_for("logged_in"))
                 message = 'Wrong password'
-                return render_template('login.html', message=message)
+                return Response(message, status=500)
         else:
             message = 'User not found'
-            return jsonify({"11":username})
+            return Response(message, status=500)
             #return render_template('login.html', message=message)
-    return jsonify({"111":"111"})
+    return Response(message, status=500)
     #return render_template('login.html', message=message)
 
 
-@app.route('/addcid', methods=["POST", "GET"])
-def addcid():
+@app.route('/addwid', methods=["POST", "GET"])
+def addwid():
     username = request.form.get("username")
-    cid = request.form.get("cid")
-    nonce = "non"
-    vip = 0
-    temp = 0
-    dataB = db.test.find_one({"username": "ooppu","aid":'sadasd.wam'})
-    if dataB is None:
-        dataA = db.test.find({"username": "ooppu"})
-        for item in dataA:
-            temp = int(item['idcount'])
-            temp +=1
-    #user = db.test.find_one({"username": username})
-            db.test.update(  {"username": username},{ '$push': { "aid": cid , "nonce": nonce , "vip": vip }})
-            db.test.update({"username": username},{'$set':{"idcount": temp}})
-            return jsonify({"yo":"add id success"})
-    else:
-        return jsonify({"yo":"dup id"})
+    wid = request.form.get("waxlist:")
+    data = json.loads(wid)
+    for i in range(len(data)) :
+        dataB = db.test.find_one({"username": username,"wid":[{'id':data[i]}]})
+        if dataB is None:
+            db.test.update_one({'username' : username},{'$push':{'wid':{'id':data[i],'vip':"0"}}} )
+            #dataA = db.test.find({"username": username})
+            #for item in dataA:
+            #    temp = int(item['idcount'])
+            #    temp +=1
+        #user = db.test.find_one({"username": username})
+            #    db.test.update(  {"username": username},{ '$push': { "aid": cid , "nonce": nonce , "vip": vip }})
+            #    db.test.update({"username": username},{'$set':{"idcount": temp}})
+            #    return jsonify({"yo":"add id success"})
+        else:
+            message = 'already have id in this user'
+            return Response(message, status=500)
+    message = 'add wam successful'
+    return Response(message, status=200)
 
 
-@app.route("/logout", methods=["POST", "GET"])
+#@app.route("/logout", methods=["POST", "GET"])
 
-def logout():
+#def logout():
     if "email" in session:
         session.pop("email", None)
         return render_template("signout.html")
     else:
         return render_template('index.html')
-#ไม่เกี่ยว
-@app.route('/test', methods=['GET'])
 
-def test():
-    db.test.find
-    return {"this is test":"testestestest"}
 #ลบไอดีalien
-#@app.route('/delaid', methods=['GET','POST'])
+@app.route('/delaid', methods=['GET','POST'])
     
-#def delaid():
+def delaid():
     user = request.form.get("username")
-    cid = request.form.get("cid")
-    dataB = db.test.find({"username":user})
-    for item in dataB :
-        temp = str(item['aid'])
-    try :
-        db.test.aggregate( [ { "$project": { "matchedIndex": { "$indexOfArray": [ "$InstructorSubject", "MongoDB" ] } } } ] )
-        db.test.update({'username':user},{ '$pull': { 'aid': cid } })
-        
-    except :
-        return jsonify({'error:':'error'})
-#showไอดีalien
-@app.route('/showaid', methods=['GET','POST'])
+    cid = request.form.get("wid")
+    dataB = db.test.find_one({"username": user,"wid":[{'id':cid}]})
+    if dataB is None:
+        db.test.update({'username' : user},{'$pull':{'wid':{'id':cid}}} )
+        message = 'delete success'
+        return Response(message, status=200)
+    else:
+        message = 'wam account not found'
+        return Response(message, status=500) 
 
-def showaid():
+
+#update nonce
+#@app.route('/wnonce', methods=['GET','POST'])
+#def wnonce():
+    non = request.form.get("nonce")
+    user = request.form.get("user")
+    wid = request.form.get("wid")
+    dataB = db.test.find_one({"username": user,"wid":[{'id':wid}]})
+    if dataB is None:
+        return jsonify({'error':'erorrororo'})
+    else:
+        db.test.update_one({'$and': [{'username':user},{'wid.id': wid}]},{'$set':{'wid.$.nonce':non}})
+        return jsonify({'success':"updatenon"})
+
+@app.route('/getwam', methods=['GET','POST'])
+def getwam():
+    user = request.form.get('username')
+    dataA = db.test.find_one({'username':user})
+    if dataA is None:
+        message = "error mai me wam"
+        return Response(message, status=500)
+        print(dataA['wid'])
+    else :
+        yo = dataA['wid']
+        yoo = json.dumps(yo)
+        return Response(yoo, status=200)
+
+@app.route('/reqnonce', methods=['GET','POST'])
+def reqnonce():
+    wid = request.form.get("wid")
+    dataA = db.testvip.find_one({"wam": wid })
+    if dataA is None:
+        message = 'no wam account in vip'
+        return Response(message, status=500)
+    else : 
+        yo = dataA['nonce']
+        yoo = json.dumps(yo)
+        return Response(yoo, status=200)
+
+#showไอดีalien
+#@app.route('/showaid', methods=['GET','POST'])
+
+#def showaid():
     user = request.form.get("username")
     dataB = db.test.find({"username":user})
     for item in dataB :
         temp = str(item['aid'])
     return jsonify({'error:':temp})
 
+@app.route('/addvip', methods=['GET','POST'])
+def addvip():
+    username = request.form.get("username")
+    wid = request.form.get("wid")
+    code = request.form.get("code")
+    dataB = db.test.find_one({"username": username,"wid.id":wid})
+    if dataB is None:
+        message= 'error'
+        return Response(message, status=500)
+    else :
+        dataA = db.testcode.find_one({ 'hash': { '$regex': code } })
+        if dataA is None:
+            message = "wrong code"
+            return Response(message, status=500)
+        else:
+            count = dataA['count']
+            leng = len(dataA['wam'])
+            if leng >= count :
+                message = "vip code already full"
+                return Response(message, status=500)
+            else : 
+                db.testcode.update_one({ 'hash': { '$regex': code } },{'$push':{'wam':'123.wam'}})
+                db.test.update_one({'$and': [{'username':username},{'wid.id': wid}]},{'$set':{'wid.$.vip':1}})
+                db.testvip.insert_one({'wam':wid,'nonce':'default',})
+                message = "add vip succesful"
+                return Response(message, status=500)
+
 @app.route('/pups', methods=['GET'])
 def pups():
     return render_template('index.html')
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0',port=8080, debug=False)
+    app.run(host='0.0.0.0',port=8080, debug=False)
