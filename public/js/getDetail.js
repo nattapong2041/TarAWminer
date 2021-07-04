41d817360746b72b6ac01ca01823fcbacd0bec0a
Nattapong Swrs <tar_afaps@outlook.com>
2021-07-04 15:53:43 +0700

    fix tlm bug

--- public/js/getDetail.js
+++ public/js/getDetail.js
@@ -34,11 +34,11 @@ async function getTLM(account) {
 
     return await fetch(url, {
         method: 'POST',
-        body: {
+        body: JSON.stringify({
             "code": "alien.worlds",
             "account": account,
             "symbol": "TLM"
-        },
+        }),
         header: {
             'content-type': 'application/json'
         }
