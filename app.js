43107e911aeb004215051976cfac6e1ba0e00cac
Nattapong Swrs <tar_afaps@outlook.com>
2021-07-13 00:31:06 +0700

    8m loop

--- app.js
+++ app.js
@@ -336,7 +336,7 @@ const doWorkWorker = async (mining_params) => {
         if (!good) {
             hash = null;
         }
-        if (itr >= 1000000 * 7) break;
+        if (itr >= 1000000 * 8) break;
     }
 
 
