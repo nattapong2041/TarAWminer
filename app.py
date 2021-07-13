a9a1d86e731a23c955c633e128657bf620c9e502
OopOopu <33390164+OopOopu@users.noreply.github.com>
2021-07-13 21:52:53 +0700

    change debug pen false

--- app.py
+++ app.py
@@ -381,4 +381,4 @@ def pups():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080, debug=True)
+    app.run(host='0.0.0.0',port=8080, debug=False)
